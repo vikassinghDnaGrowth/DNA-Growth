4cc5a374dbbb75be3f984b5b4cdba52f786c1f0b
vikassinghDnaGrowth <vikassingh.dnagrowth@gmail.com>
2024-11-09 18:44:50 +0530

    uncomment the code

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,14 @@ const App = () => {
   return (
     <>
     {/* <GsapTest/> */}
-      {/* <Navbar /> */}
+      <Navbar />
       <LandingPage />
       <Stepper />
-      {/* <Header /> */}
-      {/* <Services /> */}
+      <Header />
+      <Services />
       <ContactForm />
       <Carousel />
-      {/* <CFO /> */}
+      <CFO />
       <Help />
       <Card />
       <WhyUs />
